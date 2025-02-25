d8285cc1c0ddae48d278c2f13dd4cffe4431aafd
Juan-GR123 <juangarros2005@gmail.com>
2025-02-25 12:39:16 +0100

    carrito

--- Tarea_de_clase/tarea12/fuente/js/cesta.js
+++ Tarea_de_clase/tarea12/fuente/js/cesta.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cesta = document.querySelector(".Cesta");
   const botonComprar = document.querySelector(".comprar");
+  const botonComprar2 = document.querySelector(".comprar2");
   const botonCerrar = document.querySelector(".cerrarP");
   const mainP = document.querySelector("main");
 
@@ -11,7 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
   botonComprar.addEventListener("click", function () {
     cesta.style.display = "flex";
     cesta.style.position = "relative"; // O el valor que prefieras
-    mainP.style.display = "none"; 
+    mainP.style.display = "none";
+  });
+
+  // Mostrar la cesta al hacer clic en "Comprar"
+  botonComprar2.addEventListener("click", function () {
+    cesta.style.display = "flex";
+    cesta.style.position = "relative"; // O el valor que prefieras
+    mainP.style.display = "none";
   });
 
   // Ocultar la cesta al hacer clic en "Cerrar"
