3f088c42a7500426e5be7a27f049bffdef0ff673
Juan-GR123 <juangarros2005@gmail.com>
2025-03-03 13:50:37 +0100

    vite_hecho

--- Tarea_de_clase/tarea12/vite.config.js
+++ Tarea_de_clase/tarea12/vite.config.js
@@ -1,25 +1,29 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  appType: 'mpa', // Indica que es un proyecto multipágina
+  appType: 'mpa', // Proyecto multipágina
   base: './',
-  root: resolve(__dirname, 'fuente'), // Especifica que "fuente" es la raíz
+  root: path.resolve(__dirname, 'fuente'), // Especifica "fuente" como raíz
   build: {
     outDir: '../produccion',
-    emptyOutDir: true,    //Vacía el directorio de producción antes de compilar
+    emptyOutDir: true, // Vacía la carpeta antes de compilar
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'fuente/index.html'),
-        principal: resolve(__dirname, 'fuente/html/pagina_Principal.html'),
-        perfil: resolve(__dirname, 'fuente/html/pagina_Perfil.html'),
-        nosotros: resolve(__dirname, 'fuente/html/sobre_Nosotros.html'),
-        libros: resolve(__dirname, 'fuente/html/pagina_Libros.html'),
-        cesta: resolve(__dirname, 'fuente/html/pagina_Cesta.html'),
+        main: path.resolve(__dirname, 'fuente/index.html'),
+        principal: path.resolve(__dirname, 'fuente/html/pagina_Principal.html'),
+        perfil: path.resolve(__dirname, 'fuente/html/pagina_Perfil.html'),
+        nosotros: path.resolve(__dirname, 'fuente/html/sobre_Nosotros.html'),
+        libros: path.resolve(__dirname, 'fuente/html/pagina_Libros.html'),
+        cesta: path.resolve(__dirname, 'fuente/html/pagina_Cesta.html'),
       },
     },
-    minify: 'terser', // Asegura que JS y CSS también se minimicen
+    minify: 'terser', // Minimiza JS y CSS
   },
   plugins: [
     createHtmlPlugin({
@@ -30,7 +34,7 @@ export default defineConfig({
     open: true,
   },
   preview: {
-    open: true, // Abre automáticamente la previsualización
-    port: 4173, // Puerto para el servidor de previsualización
+    open: true,
+    port: 4173,
   },
 });
